f97c72503c2759f998852120325020581abf56d7
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-04-04 17:37:32 +0200

    Check for jq and create directory structure if needed in conversion script

--- geojson-to-elasticsearch.sh
+++ geojson-to-elasticsearch.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-geojson_dir=${1:-'./geojson'};
-elasticsearch_dir=${2:-'./elasticsearch'};
+ROOT_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
+REQUIRED_COMMANDS=(jq)
 
-cp -R $geojson_dir/* $elasticsearch_dir;
+for cmd in "${REQUIRED_COMMANDS[@]}"
+do
+    command -v ${cmd} > /dev/null 2>&1 || \
+    { echo >&2 "I require ${cmd} but it's not installed. Aborting."; exit 1 }
+done
 
-files=$(find $elasticsearch_dir -name '*.geojson');
+geojson_dir=${1:-${ROOT_DIR}/geojson}
+elasticsearch_dir=${2:-${ROOT_DIR}/elasticsearch}
 
-for file in $files; do
-  if [ -f "$file" ]; then
-    without_extension=${file%.*};
-    json_file=$without_extension.json;
-    
-    touch $json_file;
-    jq '. | {location:.geometry}' $file > $json_file;
-    
-    rm $file;
-    
-    echo "Created $json_file";
-  fi
+for file in $(find ${geojson_dir} -type f -name '*.geojson'); do
+    output_dir="$(dirname ${elasticsearch_dir}/${file#${geojson_dir}/})"
+    output_file="$(basename ${file%.geojson}.json)"
+
+    [ -d ${output_dir} ] || mkdir -p ${output_dir}
+    jq '. | {location:.geometry}' ${file} > ${output_dir}/${output_file}
+    echo "Created ${output_dir}/${output_file}"
 done
